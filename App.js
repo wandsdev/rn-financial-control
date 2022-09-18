07f5024bf24a328b90453d78f9faaab652804550
wands <wandsdeveloper@gmail.com>
2022-09-18 11:47:51 -0300

    estrutura de tabs

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { styleColor } from "./src/styles/style";
+
+const HomeScreen = () => {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Home</Text>
+		</View>
+	);
+};
+
+const PeriodScreen = () => {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Period</Text>
+		</View>
+	);
+};
+
+const PostingTypeScreen = () => {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Posting Type</Text>
+		</View>
+	);
+};
+
+const Tab = createBottomTabNavigator();
+
+const TabsNavigation = () => {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen
+				name="Home"
+				component={HomeScreen}
+				options={{
+					title: "Lançamentos",
+					tabBarLabelStyle: {
+						fontSize: 18,
+						color: styleColor.primary600,
+					},
+					tabBarIcon: () => {
+						return (
+							<Feather
+								name="trending-up"
+								size={32}
+								color={styleColor.primary600}
+							/>
+						);
+					},
+				}}
+			/>
+			<Tab.Screen
+				name="PostingType"
+				component={PostingTypeScreen}
+				options={{
+					title: "Tipos de Lançamentos",
+					tabBarLabelStyle: {
+						fontSize: 18,
+						color: styleColor.primary600,
+					},
+					tabBarIcon: () => {
+						return (
+							<Feather
+								name="trending-up"
+								size={32}
+								color={styleColor.primary600}
+							/>
+						);
+					},
+				}}
+			/>
+			<Tab.Screen name="Period" component={PeriodScreen} />
+		</Tab.Navigator>
+	);
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<StatusBar style="auto" />
+			<NavigationContainer>
+				<TabsNavigation />
+			</NavigationContainer>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		color: "purple",
+		fontSize: 32,
+		letterSpacing: 4,
+		fontWeight: "bold",
+	},
 });
---
+++ src/styles/style.js
@@ -0,0 +1,12 @@
+export const styleColor = {
+	primary50: "#faf5ff",
+	primary100: "#f3e8ff",
+	primary200: "#e9d5ff",
+	primary300: "#d8b4fe",
+	primary400: "#c084fc",
+	primary500: "#a855f7",
+	primary600: "#9333ea",
+	primary700: "#7e22ce",
+	primary800: "#6b21a8",
+	primary900: "#581c87",
+};
